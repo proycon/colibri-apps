61409539b09180b740c612744d06f5c9aae9d945
Maarten van Gompel <proycon@anaproy.nl>
2014-12-13 17:48:00 +0100

    added

---
+++ start_predictor_demo.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 predictor.py -m demo/newscommentary.en.t5l8.colibri.patternmodel -d demo/newscommentary.en.colibri.dat -c demo/newscommentary.en.colibri.cls --title="News Commentary Corpus - English"
